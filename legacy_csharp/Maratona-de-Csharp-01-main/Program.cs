d915ac98c81e91d6445b1a9add4b05aee750ef66
Ernan <90629225+Opereirareis@users.noreply.github.com>
2026-05-11 22:46:03 -0300

    Delete legacy_csharp/Maratona-de-Csharp-01-main directory

--- legacy_csharp/Maratona-de-Csharp-01-main/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿float celsius = 36f;
-const float conversao = 9f / 5f;
-Console.WriteLine($"Temperatura em Celsius: {celsius}");
-float fahrenheit = celsius * conversao + 32f;
-Console.WriteLine($"Temperatura em Fahrenheit: {fahrenheit}\n");
